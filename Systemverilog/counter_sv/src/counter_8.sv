788f23f5eecb2fca98a1c5e44f57d5ec0f65e9d2
taesoo222 <taesoo222@users.noreply.github.com>
2026-08-30 13:52:11 +0900

    Implement counter_8 module logic

--- Systemverilog/counter_sv/src/counter_8.sv
+++ Systemverilog/counter_sv/src/counter_8.sv
@@ -1,5 +1,28 @@
 `timescale 1ns / 1ps
 
-module counter_8(
-    );
+module counter (
+    input  logic       clk,
+    input  logic       rst_n,
+    input  logic       enable,
+    output logic [2:0] counter,
+    output logic       o_tick
+);
+
+    always_ff @(posedge clk) begin
+        if (!rst_n) begin
+            counter <= 1'b0;
+            o_tick  <= 1'b0;
+        end else begin
+            if (enable) begin
+                counter <= counter + 1;
+                if (counter == 7) begin
+                    o_tick <= 1'b1;
+                end else begin
+                    o_tick <= 1'b0;
+                end
+            end else o_tick <= 1'b0;
+        end
+    end
+
 endmodule
+
