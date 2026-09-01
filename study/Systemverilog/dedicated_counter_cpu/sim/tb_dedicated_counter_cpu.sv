d8df4224eb2718a4d5dc3df6b5581444a5d0e39f
taesoo222 <kts990222@gmail.com>
2026-09-01 16:58:33 +0900

    Add tb_dedicated_counter_cpu.sv

---
+++ study/Systemverilog/dedicated_counter_cpu/sim/tb_dedicated_counter_cpu.sv
@@ -0,0 +1,28 @@
+module tb_dedicated_counter_cpu ();
+
+    logic clk = 0, rst_n = 0;
+    logic [3:0] out;
+
+    always #5 clk = ~clk;
+
+    dedicated_counter_cpu dut (
+        .clk  (clk),
+        .rst_n(rst_n),
+        .out  (out)
+    );
+
+    initial begin
+        $fsdbDumpfile("wave.fsdb");
+        $fsdbDumpvars(0,tb_dedicated_counter_cpu);
+    end
+
+    initial begin
+        #10;
+        rst_n = 1;
+        
+        #500;
+        $finish;
+    end
+
+
+endmodule
